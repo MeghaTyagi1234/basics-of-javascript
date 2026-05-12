d71a35a5236a460854c2f4ee214796a86a6c3628
Megha Tyagi <smbhvtg@gmail.com>
2026-05-12 10:00:22 +0000

    conversion of datattypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+let score="33aa";
+console.log(score);
+console.log(typeof(score));
+let convertInNumber=Number(score);
+console.log(typeof(convertInNumber));
+console.log(convertInNumber);
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const customerId = 123;
+var customerAddress = "japan";
+let customerName = "mayank";
+let cityName = "ghaziabad";
+
+console.log(customerId);
+customerAddress = "china";
+console.table({ customerId, customerAddress, customerName, cityName });
